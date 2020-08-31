b4675894cea292b9b7b915e37aeb99eb46ba8e5a
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 16:34:49 -0300

    feat exercicio-lp-20

--- exercicio20.py
+++ exercicio20.py
@@ -1,4 +1,3 @@
-imprimir = print
 quilogramas  =  float ( input ("Digite o peso em Kg:" ))
-libras       =  quilogramas  /  0,45
-imprimir ( libras )
+libras       =  quilogramas / 0,45
+print ( libras )
