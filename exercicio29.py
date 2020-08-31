6bfb4dd11c0f9bc494d12c05df8de9a708f86cb7
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 16:05:03 -0300

    feat exercicio29

--- exercicio29.py
+++ exercicio29.py
@@ -1,9 +1,9 @@
 imprimir = print
 entrada = print
-n1  =  float (input ("Digite a nota (n1):"))
-n2  =  float (input ("Digite a nota (n2):"))
-n3  =  float (input ("Digite a nota (n3):"))
-n4  =  float (entrada ("Digite a nota (n4):"))
+n1    =  float (input ("Digite a nota (n1):"))
+n2    =  float (input ("Digite a nota (n2):"))
+n3    =  float (input ("Digite a nota (n3):"))
+n4    =  float (entrada ("Digite a nota (n4):"))
 soma  =  n1  +  n2  +  n3  +  n4
 resultado  =  float ( soma ) /  4
 imprimir ( resultado )
