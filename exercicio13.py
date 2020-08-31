4a76b547ac68b76ca5b64d8e6096f7bd86fd7368
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 16:26:13 -0300

    feat exercicio-lp-13

--- exercicio13.py
+++ exercicio13.py
@@ -1,5 +1,3 @@
-imprimir = print
-entrada = print
-quilometros      =  float ( entrada ("Digite a distância em KM:" ))
-milhas       =  quilometros /  1,61
-imprimir ( milhas )
+quilometros  =  float(input("Digite a distância em KM:" ))
+milhas       =  quilometros / 1,61
+print ( milhas )
