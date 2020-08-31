531997b0da63bef4264b9412d9747ae8fb6880fa
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 15:19:06 -0300

    exercicio30

---
+++ exercicio30.py
@@ -0,0 +1,4 @@
+cotacao    =  float(input ("Digite a cotação do dólar: "))
+conversao       =  float(input ("Digite o valor a ser convertido: "))
+valorFinal  =  conversao * cotacao
+impressão ( valorFinal )
