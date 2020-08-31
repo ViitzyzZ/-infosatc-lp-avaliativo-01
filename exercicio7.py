16621c2e7ed35b9027f8f70e7cdca5aedf891855
ViitzyzZ <braiantomasi0018@outlook.com>
2020-08-31 15:25:21 -0300

    feat exercicio7

--- exercicio7.py
+++ exercicio7.py
@@ -1,3 +1,3 @@
-celsius = float(input('Entre com a temperatura em graus Celsius: '))
+celsius = float(input("Entre com a temperatura em graus Celsius: "))
 far = celsius * (9 / 5) + 32
 print('Valor em Fahrenheit:' + far + "°F")
